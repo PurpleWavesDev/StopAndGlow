31a653de32bc1b57e009b2b8d3dfd72a55b36947
Kira Vogt <ko.vogt@proton.me>
2024-03-04 00:21:38 +0100

    template files

---
+++ src/renderer/fitter/fourier.py
@@ -0,0 +1,30 @@
+import taichi as ti
+import taichi.math as tm
+import taichi.types as tt
+
+        
+#def FourierSeries(order, u, v):
+#    series = np.array([1], dtype=np.float32)
+#    for n in range(1, order+1):
+#            for m in range(1, order+1):
+#                series = np.append(series, (math.cos(n*u)*math.cos(m*v)))
+#                series = np.append(series, (math.cos(n*u)*math.sin(m*v)))
+#                series = np.append(series, (math.sin(n*u)*math.cos(m*v)))
+#                series = np.append(series, (math.sin(n*u)*math.sin(m*v)))
+#    return series
+#def FourierFactorCount(order):
+#    return 4*order*order+1
+
+
+        
+@ti.func
+def sampleUVFour(A: ti.template(), y: ti.i32, x: ti.i32, u: ti.f32, v: ti.f32):
+    rgb = A[0, y, x]
+    for m, n in ti.ndrange((1, A.shape[0]), (1, A.shape[0])):
+        mult = A[m, y, x] * A[n, y, x]
+        rgb += mult * tm.cos(n*u)*tm.cos(m*v) +\
+               mult * tm.cos(n*u)*tm.sin(m*v) +\
+               mult * tm.sin(n*u)*tm.cos(m*v) +\
+               mult * tm.sin(n*u)*tm.sin(m*v)
+    return rgb
+    
---
+++ src/renderer/fitter/normal.py
@@ -0,0 +1,36 @@
+from src.renderer.fitter.pseudoinverse import PseudoinverseFitter
+from src.config import *
+
+import taichi as ti
+import taichi.math as tm
+import taichi.types as tt
+import src.ti_base as tib
+
+class NormalFitter(PseudoinverseFitter):
+    name = "Normalmap Fitter"
+    
+    def __init__(self, settings = {}):
+        super().__init__(settings)
+        # Settings
+        #self._scale_positive = settings['scale_to_positive'] if 'scale_to_positive' in settings else True
+    
+    def getCoefficientCount(self) -> int:
+        """Returns number of coefficients"""
+        return 3
+            
+    def fillLightMatrix(self, line, coord):
+        u, v = coord
+        line = [u, v, 1]
+    
+    def renderLight(self, buffer, coords, slices=1):
+        u, v = coords
+        sampleLight(buffer, self._coefficients, u, v)
+    
+    def renderHdri(self, hdri, rotation, slices=1):
+        pass
+    
+
+@ti.kernel
+def sampleLight(pix: ti.types.ndarray(dtype=tt.vector(3, ti.f32), ndim=2), coeff: ti.template(), u: ti.f32, v: ti.f32):
+    for y, x in pix:
+        pix[y, x] = [coeff[0, y, x][0], coeff[1, y, x][0], 0.5]
