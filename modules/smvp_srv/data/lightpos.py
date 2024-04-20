197a9722d263e79052a2c0b5e1b80347aa93aaa5
Kira Vogt <ko.vogt@proton.me>
2024-04-20 11:40:05 +0200

    TopAngle to ZVec which corresponds more to the UV coordinates of the top hemisphere but with the length of the latitude

--- modules/smvp_srv/data/lightpos.py
+++ modules/smvp_srv/data/lightpos.py
@@ -7,8 +7,8 @@ from ..utils import *
 
 class CoordType(Enum):
     XYZ = 0
-    ZAngles = 1
-    LatLong = 2
+    LatLong = 1
+    ZVec = 2
     Chromeball = 3
     
 
@@ -19,7 +19,7 @@ class LightPosition:
         # Latlong in radians: -pi to +pi Latitute; 0 to 2pi Longitude
         self._latlong = None
         # Angles seen from top, -pi to +pi
-        self._topangles = None
+        self._zvec = None
         # Chromeball uv coordinates (-1 to +1)
         self._chromeball = chromeball
     
@@ -35,33 +35,33 @@ class LightPosition:
             # Offsets for longitude
             if self._xyz[2] < 0: # Back side
                 longitude = (math.pi-longitude) # pi/2 to 3* pi/2, correct value
-            self._latlong = [latitude, (longitude+pi_times_2) % pi_times_2] # make longitude all positive
+            self._latlong = np.array([latitude, (longitude+pi_times_2) % pi_times_2], dtype=np.float32) # make longitude all positive
             
         return self._latlong
     
-    def getTopAngles(self): # TODO: Does that even work? Rotation order problem? Maybe solve with scalar product?
-        """Angles around X and Z axis where the zenith is (0, 0)"""
-        if self._topangles is None:
-            if self._xyz[1] > 0:
-                # Calculate
-                angle1 = math.asin(self._xyz[2]) # Orthogonal of screen
-                angle2 = math.asin(self._xyz[0]) # Horizontal component
-                self._topangles = [angle1, angle2]
+    def getZVec(self):
+        """2D Vector vec around zenith with max length PI"""
+        if self._zvec is None:
+            if self._xyz == [0,1,0]:
+                self._zvec = np.array([0,0], dtype=np.float32)
+            elif self._xyz == [0,-1,0]:
+                self._zvec = np.array([math.pi,0], dtype=np.float32) # Could be any point on the circle with radius pi
             else:
-                # Lower part of sphere
-                angle1 = pi_by_2 + math.acos(self._xyz[2]) if self._xyz[2] > 0 else -pi_by_2 - math.acos(math.abs(self._xyz[2]))
-                angle2 = pi_by_2 + math.acos(self._xyz[0]) if self._xyz[0] > 0 else -pi_by_2 - math.acos(math.abs(self._xyz[0]))
-                self._topangles = [angle1, angle2]
-        return self._topangles
+                # Vector around 0,0 with direction of xz-plane (longitude) and length of angle from zenith
+                self._zvec = np.array([self._xyz[0], self._xyz[2]], dtype=np.float32)
+                self._zvec /= math.sqrt(self._zvec[0]**2 + self._zvec[1]**2) # Normalize
+                self._zvec *= math.acos(self._xyz[1]) # Range 0 to pi (zenith is 0)
+                #alt_angle = self._zvec * (1-np.dot(self._xyz, [0,1,0]))/2 * math.pi # Alternative calculation
+        return self._zvec
     
     def getLLNorm(self) -> [float, float]:
         """Returns Lat-Long coordinates in the range of 0 to 1"""
         ll = self.getLL()
-        return [(ll[0]+pi_by_2) / math.pi, (ll[1]+math.pi) % pi_times_2 / pi_times_2]
+        return np.array([(ll[0]+pi_by_2) / math.pi, (ll[1]+math.pi) % pi_times_2 / pi_times_2], dtype=np.float32)
 
-    def getTopAnglesNorm(self) -> [float, float]:
-        """Returns Zenith Angle from -1 to 1"""
-        return self.getTopAngles() / math.pi
+    def getZVecNorm(self) -> [float, float]:
+        """Returns Zenith Angle with max length 1"""
+        return self.getZVec() / math.pi
     
     def getChromeball(self) -> [float, float]:
         return self._chromeball
@@ -70,14 +70,14 @@ class LightPosition:
         match coords:
             case CoordType.XYZ:
                 return self.getXYZ()
-            case CoordType.ZAngles:
-                if normalized:
-                    return self.getTopAnglesNorm()
-                return self.getTopAngles()
             case CoordType.LatLong:
                 if normalized:
                     return self.getLLNorm()
                 return self.getLL()
+            case CoordType.ZVec:
+                if normalized:
+                    return self.getTopAnglesNorm()
+                return self.getTopAngles()
             case CoordType.Chromeball:
                 return self.getChromeball()
 
